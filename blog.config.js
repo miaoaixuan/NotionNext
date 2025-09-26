c8023fae1fc72f694760ee61c8a3c5578564ba35
泠鹇 <20233001447@hainanu.edu.cn>
2025-09-26 20:21:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,9 +19,9 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '陈源泉', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '心有山海，静而无边。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://chenge.ink', // 网站地址
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LinkStar', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '以天地为物，筑星辰大海之梦', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.miaoaixuan.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
